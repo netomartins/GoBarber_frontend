c731ab550bebb0b24fbf4840b840bfa1ef2f7d3c
Neto Martins <netomartins2@hotmail.com>
2022-01-28 11:23:49 -0300

    config: configured global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => (<h1>Welcome</h1>);
+const App: React.FC = () => (
+<>
+<h1>Welcome</h1>
+<GlobalStyle />
+</>
 
-
+);
 
 export default App;
