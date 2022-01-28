4cb16f4989a17ab302e101166ef555affddf59d3
Neto Martins <netomartins2@hotmail.com>
2022-01-28 14:54:18 -0300

    chore: add react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
-<>
-<h1>Welcome</h1>
-<GlobalStyle />
-</>
-
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
 );
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import logoimg from '../../assets/logo.svg'
+import { Container, Content, Background } from './style';
+
+
+const SignIn: React.FC = () => (
+
+  <Container>
+    <Content>
+      <img src={logoimg} alt="GoBarber" />
+
+      <form>
+
+        <h1>Faça seu logon</h1>
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+
+    </Content>
+
+    <Background />
+
+  </Container>
+
+);
+
+export default SignIn;
+
