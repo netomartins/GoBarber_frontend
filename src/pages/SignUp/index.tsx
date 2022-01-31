942afe3fabe2cf28e89d7ceb1dad39bc4abfa6ec
Neto Martins <netomartins2@hotmail.com>
2022-01-31 17:24:56 -0300

    config: displaying errors in input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErros';
 import logoimg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,23 +12,28 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './style';
 
 const SignUp: React.FC = () => {
-  const handleSubmit = useCallback(async (data: unknown) => {
+  const formRef = useRef<FormHandles>(null);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object) => {
     console.log(data);
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('Email obrigatório')
           .email('Digite um email válido'),
-        password: Yup.string()
-          .required('Senha obrigatória')
-          .min(6, 'No mínimo 6 dígitos'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
       await schema.validate(data, {
         abortEarly: false,
       });
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
+
+      const errors = getValidationErrors(err as Yup.ValidationError);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -36,7 +43,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoimg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
