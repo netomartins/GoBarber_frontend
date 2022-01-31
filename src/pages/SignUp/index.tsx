56feada2b9ab71962d4ecccb8cb38255cbbd31e6
Neto Martins <netomartins2@hotmail.com>
2022-01-31 15:55:28 -0300

    config: registration validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,55 +1,57 @@
-/* eslint-disable react/jsx-no-bind */
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
-import { Form } from '@unform/web'
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoimg from '../../assets/logo.svg';
-import { Container, Content, Background } from './style';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { Container, Content, Background } from './style';
 
 const SignUp: React.FC = () => {
-
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  // eslint-disable-next-line react/jsx-no-bind
-  function handleSubmit(data: object ): void {
+  const handleSubmit = useCallback(async (data: unknown) => {
     console.log(data);
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string()
+          .required('Senha obrigatória')
+          .min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-  }
   return (
-
     <Container>
       <Background />
       <Content>
-
-
         <img src={logoimg} alt="GoBarber" />
 
         <Form onSubmit={handleSubmit}>
-
           <h1>Faça seu Cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input name="password" icon={FiLock} placeholder="Senha" />
 
           <Button type="submit">Cadastrar</Button>
-
         </Form>
 
         <a href="forgot">
           <FiArrowLeft />
           Voltar para logon
         </a>
-
       </Content>
-
     </Container>
   );
-
-}
+};
 
 export default SignUp;
-
