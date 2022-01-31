25a6188c0e4d7ab7ff9f9bd59f38b3ddcee175b8
Neto Martins <netomartins2@hotmail.com>
2022-01-31 10:22:15 -0300

    config: registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
+
     <GlobalStyle />
   </>
 );
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft} from 'react-icons/fi';
+import logoimg from '../../assets/logo.svg';
+import { Container, Content, Background } from './style';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
+const SignUp: React.FC = () => (
+
+  <Container>
+    <Background />
+    <Content>
+
+
+      <img src={logoimg} alt="GoBarber" />
+
+      <form>
+
+        <h1>Faça seu Cadastro</h1>
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} placeholder="Senha" />
+
+        <Button type="submit">Cadastrar</Button>
+
+      </form>
+
+      <a href="forgot">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+
+
+    </Content>
+
+
+
+  </Container>
+
+);
+
+export default SignUp;
+
