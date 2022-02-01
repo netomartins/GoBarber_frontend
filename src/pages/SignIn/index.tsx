7c7515a2b061c9620a270e86951d68f7e097ca45
Neto Martins <netomartins2@hotmail.com>
2022-02-01 15:39:28 -0300

    config: Login by context and keeping the user in storage

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -14,17 +14,20 @@ import { Container, Content, Background } from './style';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
 
 
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const {user, signIn } = useContext(AuthContext);
+  console.log(user)
 
-
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     console.log(data);
     try {
       formRef.current?.setErrors({});
@@ -38,7 +41,11 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      signIn();
+      signIn({
+        email: data.email,
+        password: data.password
+
+      });
     } catch (err) {
       console.log(err);
 
