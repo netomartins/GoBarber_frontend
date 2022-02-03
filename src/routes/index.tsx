f9c8721fe8bac0746e06775227fad9f401dab5e4
Neto Martins <netomartins2@hotmail.com>
2022-02-03 09:22:31 -0300

    config: authenticated routes ok

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" component={SignUp} />
+    <Route path="/" exact component={SignIn} isPrivate={false} />
+    <Route path="/signup" component={SignUp} isPrivate={false} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
---
+++ src/routes/route.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as RactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/Auth';
+
+interface RouteProps extends RactDOMRouteProps {
+  isPrivate: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
