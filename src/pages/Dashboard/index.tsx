0a9f7a0d6f23a969aeb75024c4ecf5274ffa71d4
Neto Martins <netomartins2@hotmail.com>
2022-02-15 10:08:40 -0300

    config: creating header and logout button

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './style';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoimg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoimg} alt="GoBarber" />
+          <Profile>
+            <img
+              src="https://avatars.githubusercontent.com/u/20827326?v=4"
+              alt="Neto Martins"
+            />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>Neto Martins</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
